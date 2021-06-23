181341a99ab987f9d439c918ab3b44287731ceb6
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-06-22 20:04:39 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -38,7 +38,7 @@ ${negro}║${verde}             I             ${amarillo}Ético${verde}         
 ${negro}║${verde}                                                      ${negro}║${verde}
 ${negro}║${verde} ${negro}[${cian}>${negro}] ${negro}[${cian}>${negro}]${cian} ${negro}[${cian}>${negro}]${blanco} ${DATE} ${negro}[${cian}<${negro}] ${negro}[${cian}<${negro}] ${negro}[${cian}<${negro}] ${negro}║${verde} 
 ${negro}╚══════════════════════════════════════════════════════╝${verde}
-${blanco}" | pv -qL 95
+${blanco}" | pv -qL 2998
 PS1="\[\e[1;37m╭━━━( \033[1;30m@\033[1;32mScorpio28\033[1;31m\w\033[1;37m )━━━๏\n│\n╰━━━═>>> "
 }
 ctrl_c(){
