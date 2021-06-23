751d4ad26708c168cc549d304adc3a52a149d057
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-06-23 00:37:07 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -15,30 +15,30 @@ blanco="\e[1;37m"
 Scorpio28(){
 	sleep 0.5
 	clear
-echo -e "${negro}"
-echo -e "${negro}╔══════════════════════════════════════════════════════╗${verde}"
-echo -e "${negro}║${verde}   ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}]${blanco} 🎭 S C O R P I O 2 8 🎭 ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}]${verde}    ${negro}║"
-echo -e "${negro}║${verde}                                                      ${negro}║${verde}"
-echo -e "${negro}║${verde}           . .IIIII             .II                   ${negro}║${verde}"
-echo -e "${negro}║${verde}  IIIIIII. I  II  .    II..IIIIIIIIIIIIIIIIIIII       ${negro}║${verde}"
-echo -e "${negro}║${verde} .  .IIIIII  II          III ${blanco}Scorpio28${verde} IIIIIIIIII.    ${negro}║${verde}"
-echo -e "${negro}║${verde}    .IIIII.III I      IIIIIIIII ${blanco}The${verde} IIIIIIIII  I.     ${negro}║${verde}"
-echo -e "${negro}║${verde}   .IIIIII ${blanco}Hacking${verde} II  .IIII ${blanco}Mexicano${verde} IIII. III       ${negro}║${verde}"
-echo -e "${negro}║${verde}    IIIIIII ${blanco}From${verde}    ' IIIII I IIIIIIIIIIII III I      ${negro}║${verde}"
-echo -e "${negro}║${verde}    .II    ${blanco}Termux${verde}      IIIIIIIIIIII  IIIIIIIIII       ${negro}║${verde}"
-echo -e "${negro}║${verde}       I.           .IIIIIIIIIIII   I   II  I         ${negro}║${verde}"
-echo -e "${negro}║${verde}         .IIII        IIIIIIIIIIII     .       I      ${negro}║${verde}"
-echo -e "${negro}║${verde}          IIIII.          IIIIII            . I.      ${negro}║${verde}"
-echo -e "${negro}║${verde}         IIIIIIIII         IIIII            ..I  II.  ${negro}║${verde}"
-echo -e "${negro}║${verde}          IIIIIII          IIII..             IIQII   ${negro}║${verde}"
-echo -e "${negro}║${verde}            IIII           III. I            IIIEIII  ${negro}║${verde}"
-echo -e "${negro}║${verde}            III             I                I  IPI   ${negro}║${verde}"
-echo -e "${negro}║${verde}             II       ${negro}[${rojo}+${negro}] ${blanco}Hacking ${negro}[${rojo}+${negro}]${verde}        D   .    ${negro}║${verde}"
-echo -e "${negro}║${verde}             I             ${amarillo}Ético${verde}                      ${negro}║${verde}"
-echo -e "${negro}║${verde}                                                      ${negro}║${verde}"
-echo -e "${negro}║${verde} ${negro}[${cian}>${negro}] ${negro}[${cian}>${negro}]${cian} ${negro}[${cian}>${negro}]${blanco} ${DATE} ${negro}[${cian}<${negro}] ${negro}[${cian}<${negro}] ${negro}[${cian}<${negro}] ${negro}║${verde}" 
-echo -e "${negro}╚══════════════════════════════════════════════════════╝${verde}
-${blanco}" | pv -qL 1888
+echo -e "${negro}
+${negro}╔══════════════════════════════════════════════════════╗${verde}
+${negro}║${verde}   ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}]${blanco} 🎭 S C O R P I O 2 8 🎭 ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}] ${negro}[${cian}+${negro}]${verde}    ${negro}║
+${negro}║${verde}                                                      ${negro}║${verde}
+${negro}║${verde}           . .IIIII             .II                   ${negro}║${verde}
+${negro}║${verde}  IIIIIII. I  II  .    II..IIIIIIIIIIIIIIIIIIII       ${negro}║${verde}
+${negro}║${verde} .  .IIIIII  II          III ${blanco}Scorpio28${verde} IIIIIIIIII.    ${negro}║${verde}
+${negro}║${verde}    .IIIII.III I      IIIIIIIII ${blanco}The${verde} IIIIIIIII  I.     ${negro}║${verde}
+${negro}║${verde}   .IIIIII ${blanco}Hacking${verde} II  .IIII ${blanco}Mexicano${verde} IIII. III       ${negro}║${verde}
+${negro}║${verde}    IIIIIII ${blanco}From${verde}    ' IIIII I IIIIIIIIIIII III I      ${negro}║${verde}
+${negro}║${verde}    .II    ${blanco}Termux${verde}      IIIIIIIIIIII  IIIIIIIIII       ${negro}║${verde}
+${negro}║${verde}       I.           .IIIIIIIIIIII   I   II  I         ${negro}║${verde}
+${negro}║${verde}         .IIII        IIIIIIIIIIII     .       I      ${negro}║${verde}
+${negro}║${verde}          IIIII.          IIIIII            . I.      ${negro}║${verde}
+${negro}║${verde}         IIIIIIIII         IIIII            ..I  II.  ${negro}║${verde}
+${negro}║${verde}          IIIIIII          IIII..             IIQII   ${negro}║${verde}
+${negro}║${verde}            IIII           III. I            IIIEIII  ${negro}║${verde}
+${negro}║${verde}            III             I                I  IPI   ${negro}║${verde}
+${negro}║${verde}             II       ${negro}[${rojo}+${negro}] ${blanco}Hacking ${negro}[${rojo}+${negro}]${verde}        D   .    ${negro}║${verde}
+${negro}║${verde}             I             ${amarillo}Ético${verde}                      ${negro}║${verde}
+${negro}║${verde}                                                      ${negro}║${verde}
+${negro}║${verde} ${negro}[${cian}>${negro}] ${negro}[${cian}>${negro}]${cian} ${negro}[${cian}>${negro}]${blanco} ${DATE} ${negro}[${cian}<${negro}] ${negro}[${cian}<${negro}] ${negro}[${cian}<${negro}] ${negro}║${verde} 
+${negro}╚══════════════════════════════════════════════════════╝${verde}
+${blanco}" | pv -qL 7888
 PS1="\[\e[0m╭━━━( \033[1;30m@\033[1;32mScorpio28\033[0;35m\w\033[0m )━━━๏\n│\n╰━━━═>>> "
 }
 ctrl_c(){
